016ef07a6803440a2a9476e9691c8cd43335e3d3
K Sunder Mishra <73066899+realksm@users.noreply.github.com>
2023-10-02 23:16:21 +0530

    Time: 5 ms (29.36%), Space: 49.9 MB (22.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,18 +1,10 @@
 class Solution {
-    class Pair {
-        int row;
-        int col;
-        Pair(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-    }
     public int numIslands(char[][] grid) {
-        boolean[][] vis = new boolean[grid.length][grid[0].length];
         int ans = 0;
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[0].length; j++) {
-                if(!vis[i][j] && grid[i][j] == '1') {
+                if(grid[i][j] == '1' && !vis[i][j]) {
                     ans++;
                     bfs(grid, i, j, vis);
                 }
@@ -20,27 +12,25 @@ class Solution {
         }
         return ans;
     }
-    public void bfs(char grid[][], int i, int j, boolean[][] vis) {
+    public void bfs(char[][] grid, int i, int j, boolean[][] vis) {
         vis[i][j] = true;
-        Queue<Pair> q = new LinkedList<Pair>();
-        q.add(new Pair(i, j));
-        int n = grid.length;
-        int m = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i, j});
         int[] dy = {-1, 1, 0, 0};
         int[] dx = {0, 0, -1, 1};
-
+        int n = grid.length;
+        int m = grid[0].length;
         while(!q.isEmpty()) {
-            int row = q.peek().row;
-            int col = q.peek().col;
-            q.remove();
+            int row = q.peek()[0];
+            int col = q.poll()[1];
+
             for(int k = 0; k < 4; k++) {
-              int nr = row + dy[k];
-              int nc = col + dx[k];
-              if(nr >= 0 && nr < n && nc >= 0 && nc < m
-               && grid[nr][nc] == '1' && !vis[nr][nc]) {
-                vis[nr][nc] = true;
-                q.add(new Pair(nr, nc));
-              }
+                int nr = row + dy[k];
+                int nc = col + dx[k];
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1' && !vis[nr][nc]) {
+                    vis[nr][nc] = true;
+                    q.add(new int[]{nr,nc});
+                }
             }
         }
     }
