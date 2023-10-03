ed0b21d04b0ff0f6d43ab8abb0e408e6ecbb6f65
K Sunder Mishra <73066899+realksm@users.noreply.github.com>
2023-10-03 22:35:19 +0530

    Time: 2 ms (14.7%), Space: 43.6 MB (96.5%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n = image.length, m = image[0].length;
+        boolean[][] vis = new boolean[n][m];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{sr, sc});
+        int point = image[sr][sc];
+        image[sr][sc] = color;
+        vis[sr][sc] = true;
+
+        int[] dy = {-1, 1, 0, 0};
+        int[] dx = {0, 0, 1, -1};
+        while(!q.isEmpty()) {
+            int r = q.peek()[0];
+            int c = q.poll()[1];
+
+            for(int k = 0; k < 4; k++) {
+                int nr = r + dy[k];
+                int nc = c + dx[k];
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && image[nr][nc] == point && !vis[nr][nc]) {
+                    q.add(new int[]{nr, nc});
+                    image[nr][nc] = color;
+                    vis[nr][nc] = true;
+                }
+            }
+        }
+        return image;
+    }
+}
