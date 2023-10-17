62c7298e6da8087c8cd3eaaf3d5d7684f67ecefd
K Sunder Mishra <73066899+realksm@users.noreply.github.com>
2023-10-17 20:38:07 +0530

    Time: 58 ms (60.39%), Space: 45.1 MB (8.44%) - LeetHub

--- 2789-counter-ii/2789-counter-ii.js
+++ 2789-counter-ii/2789-counter-ii.js
@@ -2,13 +2,29 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
+ class Counter {
+  constructor(init) {
+    this.init = init;
+    this.presentCount = init;
+  }
+
+  increment() {
+    this.presentCount += 1;
+    return this.presentCount;
+  }
+
+  decrement() {
+    this.presentCount -= 1;
+    return this.presentCount;
+  }
+
+  reset() {
+    this.presentCount = this.init;
+    return this.presentCount;
+  }
+}
 var createCounter = function(init) {
-    let counter = init;
-    return {
-        increment: () => ++counter,
-        decrement: () => --counter,
-        reset: () => counter = init,
-    }
+    return new Counter(init);
 };
 
 /**
