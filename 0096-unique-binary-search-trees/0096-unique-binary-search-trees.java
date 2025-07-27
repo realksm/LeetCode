55dc84f501be73cbe9faec30161b0d08cbb784c1
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-07-27 16:15:24 +0530

    Time: 0 ms (100%), Space: 40.6 MB (36.67%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        return dfs(n, dp);
+    }
+
+    private int dfs(int n, int[] dp) {
+        if(n <= 1) {
+            return (dp[n] = 1);
+        }
+
+        if(dp[n] != 0) return dp[n];
+
+        for(int i = 1; i <= n; i++) {
+            int left = dfs(i - 1, dp);
+            int right = dfs(n - i, dp);
+            dp[n] += left * right;
+        }
+
+        return dp[n];
+    }
+}
