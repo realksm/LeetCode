189d8fc813efcf500579183832da77a7a7a20789
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-07-27 17:12:11 +0530

    Time: 3 ms (92.61%), Space: 44.9 MB (33.03%) - LeetHub

---
+++ 0935-orderly-queue/0935-orderly-queue.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String orderlyQueue(String s, int k) {
+        if(k > 1){
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            return new String(ch);
+        }
+
+        StringBuilder sb = new StringBuilder(s);
+        sb.append(sb.charAt(0));
+        sb.deleteCharAt(0);
+        
+        String best = s;
+        while(!sb.toString().equals(s)) {
+            if(findBest(best, sb.toString())) best = sb.toString();
+            sb.append(sb.charAt(0));
+            sb.deleteCharAt(0);
+        }
+
+        return best;
+    }
+
+    private boolean findBest(String best, String s) {
+        int i = 0;
+        while(i < best.length()) {
+            if(best.charAt(i) < s.charAt(i)) {
+                return false;
+            } else if(best.charAt(i) > s.charAt(i)) {
+                return true;
+            }
+            i++;
+        }
+        return true;
+    }
+}
