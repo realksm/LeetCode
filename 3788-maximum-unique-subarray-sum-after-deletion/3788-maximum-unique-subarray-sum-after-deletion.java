3f2f87b6ea1e94a8aa6f4bdb5248500a1730cf52
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-07-31 20:41:16 +0530

    Time: 1 ms (99.89%), Space: 42.5 MB (57.79%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -1,18 +1,17 @@
 class Solution {
     public int maxSum(int[] nums) {
         boolean[] ifPresent = new boolean[101];
-        int sum = 0, flag = -1;
+        int sum = 0;
+        boolean flag = false;
         int max = -101;
         for(int i : nums) {
             if(i >= 0 && !ifPresent[i]) {
-                flag = 0;
+                flag = true;
                 sum += i;
                 ifPresent[i] = true;
             }
-            if(i < 0) {
-                max = Math.max(max, i);
-            }
+            max = Math.max(max, i);
         }
-        return flag == 0 ? sum : max;
+        return flag ? sum : max;
     }
 }
