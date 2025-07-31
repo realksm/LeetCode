0b6e54ff3e82868c90c7c8b492cb71f0d77e8eee
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-07-31 21:07:22 +0530

    Time: 48 ms (36.23%), Space: 42 MB (27.91%) - LeetHub

---
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    int[][] dp;
+    public int getMoneyAmount(int n) {
+        dp = new int[n + 1][n + 1];
+        return minimax(1, n);
+    }
+
+    public int minimax(int start, int end) {
+        if(start >= end) return 0;
+        if(dp[start][end] != 0) return dp[start][end];
+        int min = Integer.MAX_VALUE;
+
+        for(int i = start; i <= end; i++) {
+            int left = minimax(start, i - 1);
+            int right = minimax(i + 1, end);
+            min = Math.min(i + Math.max(left, right), min);
+        }
+        return (dp[start][end] = min);
+    }
+}
