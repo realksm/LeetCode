b44df75756994358671648a8c9be7c7ee2e10284
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-08-02 20:45:08 +0530

    Time: 2 ms (97.96%), Space: 68.3 MB (5.31%) - LeetHub

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.java
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.java
@@ -1,9 +1,19 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        Arrays.sort(nums);
-        for(int i = 1; i < nums.length; i += 2) {
-            nums[i] = nums[i] + nums[i - 1] - (nums[i - 1] = nums[i]);
+        // Arrays.sort(nums);
+        // for(int i = 1; i < nums.length; i += 2) {
+        //     nums[i] = nums[i] + nums[i - 1] - (nums[i - 1] = nums[i]);
+        // }
+        // return nums;
+
+        int temp;
+        for(int i = 1; i < nums.length - 1; i++) {
+            if(nums[i] == (nums[i - 1] + nums[i + 1]) / 2) {
+                nums[i] = nums[i] + nums[i + 1] - (nums[i + 1] = nums[i]);
+                if(i >= 2) i -= 2;
+            }
         }
+        
         return nums;
     }
 }
