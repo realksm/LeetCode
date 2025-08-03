54a9b9cde2d6a92db4f005e9adf3005dcae264ee
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-08-03 21:22:31 +0530

    Time: 3 ms (74.68%), Space: 106.3 MB (29.11%) - LeetHub

---
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        int max = 0 , sum = 0, left = 0;
+        for(int right = 0; right < fruits.length; right++) {
+            sum += fruits[right][1];
+            while(left <= right && !canReach(fruits[left][0], fruits[right][0], startPos, k)) {
+                sum -= fruits[left][1];
+                left++;
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+
+    public boolean canReach(int left, int right, int start, int k) {
+        if(right <= start) return start - left <= k;
+        if(left >= start) return right - start <= k;
+        int leftDist = start - left, rightDist = right - start;
+        int min = Math.min(2 * leftDist + rightDist, 2 * rightDist + leftDist);
+        return min <= k;
+    } 
+}
