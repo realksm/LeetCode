888ec9d250167f457e8c30d4d48495704c17a65a
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-08-03 22:06:55 +0530

    Time: 0 ms (100%), Space: 42.2 MB (55.54%) - LeetHub

--- 3311-ant-on-the-boundary/3311-ant-on-the-boundary.java
+++ 3311-ant-on-the-boundary/3311-ant-on-the-boundary.java
@@ -1,10 +1,10 @@
 class Solution {
     public int returnToBoundaryCount(int[] nums) {
-        int[] prefix = new int[nums.length];
-        prefix[0] = nums[0];
-        for(int i = 1; i < nums.length; i++) prefix[i] = prefix[i - 1] + nums[i];
-        int count = 0;
-        for(int i : prefix) if(i == 0) count++;
+        int count = 0, sum = 0;
+        for(int i : nums) {
+            sum += i;
+            if(sum == 0) count++;
+        }
         return count;
     }
 }
