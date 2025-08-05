42616032da0c5223a6947980fb9223be9d867421
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-08-05 17:33:33 +0530

    Time: 0 ms (100%), Space: 41.7 MB (87.71%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,17 +1,25 @@
 class Solution {
     public int numDecodings(String s) {
-        if(s == null || s.length() == 0) return 1;
         int n = s.length();
+        int next = 1;
+        int curr = isValid(s.charAt(n - 1)) ? 1 : 0;
 
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = s.charAt(0) != '0' ? 1 : 0;
-        for(int i = 2; i <= n; i++) {
-            int one = s.charAt(i - 1) - '0';
-            int two = 10 * (s.charAt(i - 2) - '0') + one;
-            if(one >= 1 && one <= 9) dp[i] += dp[i - 1];
-            if(two >= 10 && two <= 26) dp[i] += dp[i - 2];
+        for (int i = n - 2; i >= 0; --i) {
+            int temp = 0;
+            if (isValid(s.charAt(i))) temp += curr;
+            if (isValid(s.charAt(i), s.charAt(i + 1))) temp += next;
+            next = curr;
+            curr = temp;
         }
-        return dp[n];
+
+        return curr;
+    }
+
+    private boolean isValid(char c) {
+        return c != '0';
+    }
+
+    private boolean isValid(char c1, char c2) {
+        return c1 == '1' || c1 == '2' && c2 < '7';
     }
 }
