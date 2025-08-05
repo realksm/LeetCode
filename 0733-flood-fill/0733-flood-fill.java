7fb9418c6fde56ddcaf00708f51484e6e2872ded
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-08-05 17:53:16 +0530

    Time: 0 ms (100%), Space: 45.2 MB (19.16%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,29 +1,23 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n = image.length, m = image[0].length;
-        boolean[][] vis = new boolean[n][m];
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{sr, sc});
-        int point = image[sr][sc];
-        image[sr][sc] = color;
-        vis[sr][sc] = true;
-
-        int[] dy = {-1, 1, 0, 0};
-        int[] dx = {0, 0, 1, -1};
-        while(!q.isEmpty()) {
-            int r = q.peek()[0];
-            int c = q.poll()[1];
-
-            for(int k = 0; k < 4; k++) {
-                int nr = r + dy[k];
-                int nc = c + dx[k];
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m && image[nr][nc] == point && !vis[nr][nc]) {
-                    q.add(new int[]{nr, nc});
-                    image[nr][nc] = color;
-                    vis[nr][nc] = true;
-                }
-            }
+    public void helper(int[][] image, int sr, int sc, int color, boolean vis[][], int origiCol){
+        if(sr < 0 || sc < 0 
+            || sr >= image.length 
+            || sc >= image[0].length 
+            || vis[sr][sc] 
+            || image[sr][sc] != origiCol){
+            return;
         }
-        return image;
+        vis[sr][sc]=true;
+        image[sr][sc]=color;
+        helper(image, sr, sc - 1,color,vis,origiCol);
+        helper(image, sr, sc + 1,color,vis,origiCol);
+        helper(image, sr - 1, sc,color,vis,origiCol);
+        helper(image, sr + 1, sc,color,vis,origiCol);
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+      boolean vis[][]= new boolean[image.length][image[0].length];
+      helper(image, sr,sc,color,vis,image[sr][sc]);
+      return image;  
     }
 }
