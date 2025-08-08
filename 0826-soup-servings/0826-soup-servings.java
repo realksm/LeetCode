a77e9549dc05078baa001eaa1dad0c568fcb432d
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-08-08 16:47:46 +0530

    Time: 0 ms (100%), Space: 40.7 MB (99.4%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.java
+++ 0826-soup-servings/0826-soup-servings.java
@@ -1,5 +1,5 @@
 class Solution {
-    private double[][] memo = new double[172][172];
+    private static double[][] memo = new double[172][172];
     public double soupServings(int n) {
         return n >= 4276 ? 1 : dfs((n + 24)/25, (n + 24)/25);
     }
@@ -8,9 +8,7 @@ class Solution {
         if(a <= 0 && b <= 0) return 0.5;
         if(a <= 0) return 1.0;
         if(b <= 0) return 0.0;
-
         if(memo[a][b] > 0) return memo[a][b];
-
         return memo[a][b] = 0.25 * (dfs(a - 1, b - 3) + dfs(a - 2, b - 2) + dfs(a - 3, b - 1) + dfs(a - 4, b));
     }
 }
