a3eeae3033046637a83ce75db6c7ddc30d2c075d
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-08-10 16:11:41 +0530

    Time: 1 ms (85.79%), Space: 40.4 MB (91.58%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.java
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean reorderedPowerOf2(int n) {
-        int targetSignature = digitSignature(n);
-        for (int i = 0; i < 30; i++) {
-            if (digitSignature(1 << i) == targetSignature) {
+        int[] cnt = digitCount(n);
+        for (int i = 0; i < 31; i++) {
+            if (Arrays.equals(cnt, digitCount(1 << i))) {
                 return true;
             }
         }
         return false;
     }
 
-    private int digitSignature(int n) {
-        int signature = 0;
-        while (n > 0) {
-            signature += 1 << (n % 10);
-            n /= 10;
+    private int[] digitCount(int x) {
+        int[] count = new int[10];
+        while (x > 0) {
+            count[x % 10]++;
+            x /= 10;
         }
-        return signature;
+        return count;
     }
 }
