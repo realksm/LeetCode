a915c82d3dc28ca014ec29ef9c235122245c5e7d
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-08-18 21:47:47 +0530

    Time: 1 ms (91.58%), Space: 42.4 MB (81.82%) - LeetHub

--- 0679-24-game/0679-24-game.java
+++ 0679-24-game/0679-24-game.java
@@ -1,34 +1,53 @@
-class Solution {
+public class Solution {
     public boolean judgePoint24(int[] nums) {
-        List<Double> doubleNums = new ArrayList<>();
-
-        for (final int num : nums)
-            doubleNums.add((double) num);
-
-        return dfs(doubleNums);
+        double[] arr = new double[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = nums[i];
+        }
+        return dfs(arr, nums.length);
     }
 
-    private boolean dfs(List<Double> nums) {
-        if (nums.size() == 1)
-            return Math.abs(nums.get(0) - 24.0) < 0.001;
+    private boolean dfs(double[] nums, int n) {
+        if (n == 1) {
+            return Math.abs(nums[0] - 24.0) < 1e-3;
+        }
 
-        for (int i = 0; i < nums.size(); ++i)
-            for (int j = i + 1; j < nums.size(); ++j)
-                for (final double num : generate(nums.get(i), nums.get(j))) {
-                    List<Double> nextRound = new ArrayList<>(List.of(num));
-                    for (int k = 0; k < nums.size(); ++k) {
-                        if (k == i || k == j) // It is used in `generate()`.
-                            continue;
-                        nextRound.add(nums.get(k));
+        // Try every pair of numbers
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                double a = nums[i];
+                double b = nums[j];
+
+                // store old nums for backtracking
+                double[] next = new double[n - 1];
+                int idx = 0;
+                for (int k = 0; k < n; k++) {
+                    if (k != i && k != j) {
+                        next[idx++] = nums[k];
                     }
-                    if (dfs(nextRound))
-                        return true;
                 }
 
+                // Try all possible results
+                for (double val : generate(a, b)) {
+                    next[idx] = val;
+                    if (dfs(next, n - 1)) {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 
     private double[] generate(double a, double b) {
-        return new double[] { a * b, a / b, b / a, a + b, a - b, b - a };
+        double eps = 1e-6;
+        return new double[]{
+            a + b,
+            a - b,
+            b - a,
+            a * b,
+            Math.abs(b) < eps ? Double.NaN : a / b,
+            Math.abs(a) < eps ? Double.NaN : b / a
+        };
     }
 }
