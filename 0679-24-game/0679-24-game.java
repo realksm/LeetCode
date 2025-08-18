665d11f7d2806850066b50497cdce6d9f38236fe
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-08-18 21:59:27 +0530

    Time: 1 ms (91.58%), Space: 42.3 MB (81.82%) - LeetHub

--- 0679-24-game/0679-24-game.java
+++ 0679-24-game/0679-24-game.java
@@ -1,6 +1,5 @@
 class Solution {
     private static final double EPS = 1e-6;
-
     public boolean judgePoint24(int[] nums) {
         double[] A = new double[nums.length];
         for (int i = 0; i < nums.length; i++) A[i] = nums[i];
@@ -9,13 +8,13 @@ class Solution {
 
     private boolean dfs(double[] A, int n) {
         if (n == 1) return Math.abs(A[0] - 24) < EPS;
-
+        double[] ops;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 double a = A[i], b = A[j];
                 A[j] = A[n - 1];
 
-                double[] ops = {a + b, a * b, a - b, b - a,
+                ops = new double[] {a + b, a * b, a - b, b - a,
                                 Math.abs(b) > EPS ? a / b : Double.NaN,
                                 Math.abs(a) > EPS ? b / a : Double.NaN};
 
@@ -26,7 +25,7 @@ class Solution {
                     }
                 }
 
-                A[i] = a; A[j] = b; // restore
+                A[i] = a; A[j] = b;
             }
         }
         return false;
