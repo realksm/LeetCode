6d15cd3ab851e77d70e8e48e34b4d48ae53c0426
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-08-30 14:15:26 +0530

    Time: 1 ms (99.87%), Space: 44.2 MB (95.64%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int[] rowMask = new int[9];
-        int[] colMask = new int[9];
-        int[] boxMask = new int[9];
+        int[][] mask = new int[3][9]; // 0=row, 1=col, 2=box
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 char c = board[i][j];
-                if (c == '.') continue;
+                if (c == '.')
+                    continue;
 
-                int bit = 1 << (c - '0'); 
+                int bit = 1 << (c - '0');
                 int box = (i / 3) * 3 + (j / 3);
 
-                if ((rowMask[i] & bit) != 0 ||
-                    (colMask[j] & bit) != 0 ||
-                    (boxMask[box] & bit) != 0) return false;
+                if ((mask[0][i] & bit) != 0 ||
+                        (mask[1][j] & bit) != 0 ||
+                        (mask[2][box] & bit) != 0)
+                    return false;
 
-                rowMask[i] |= bit;
-                colMask[j] |= bit;
-                boxMask[box] |= bit;
+                mask[0][i] |= bit;
+                mask[1][j] |= bit;
+                mask[2][box] |= bit;
             }
         }
         return true;
