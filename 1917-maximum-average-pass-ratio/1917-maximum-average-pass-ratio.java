8d64bfbdf1ae3ad5aabb4c9c5388cb4ed5220902
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-09-01 21:11:27 +0530

    Time: 321 ms (88.08%), Space: 95.7 MB (60.62%) - LeetHub

---
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double maxAverageRatio(int[][] classes, int extraStudents) {
+        PriorityQueue<double[]> pq = new PriorityQueue<>(
+                (a, b) -> Double.compare(b[0], a[0])
+        );
+
+        for (int[] c : classes) {
+            double pass = c[0], total = c[1];
+            pq.offer(new double[] { gain(pass, total), pass, total });
+        }
+
+        while (extraStudents-- > 0) {
+            double[] top = pq.poll();
+            double pass = top[1] + 1, total = top[2] + 1;
+            pq.offer(new double[] { gain(pass, total), pass, total });
+        }
+
+        double sum = 0;
+        for (double[] c : pq)
+            sum += c[1] / c[2];
+        return sum / classes.length;
+    }
+
+    private double gain(double pass, double total) {
+        return (pass + 1) / (total + 1) - pass / total;
+    }
+}
