27d987569fa15fa51c6de0420025ffa5df26ae85
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-09-13 13:07:27 +0530

    Time: 1 ms (100%), Space: 42.6 MB (52.72%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -5,7 +5,7 @@ class Solution {
         for(char c : s.toCharArray()) {
             freq[c - 'a']++;
             int idx = (((1 << (c - 'a')) & 0x104111) != 0) ? 1 : 0;
-            if (freq[c - 'a'] > max[idx]) max[idx] = freq[c - 'a'];
+            max[idx] = Math.max(max[idx], freq[c - 'a']);
         }
         return max[0] + max[1];
     }
